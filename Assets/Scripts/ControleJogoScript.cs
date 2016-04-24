60b9fa3c657fb4da58b5421208f6c8340d5eee44
Alan Diego Darold <alnhcl1@gmail.com>
2016-04-24 11:47:25 -0300

    Canvas com Erro
    preciso de ajuda para terminar o  canvas

--- Assets/Scripts/ControleJogoScript.cs
+++ Assets/Scripts/ControleJogoScript.cs
@@ -6,6 +6,7 @@ public class ControleJogoScript : MonoBehaviour
 	public static bool Iniciado = false;
 
 	private static GameObject cnvJogo;
+	private ControleVidaScript controleVida;
 
 	static ControleJogoScript ()
 	{
@@ -21,4 +22,14 @@ public class ControleJogoScript : MonoBehaviour
 		cnvInicio.SetActive (false);
 		cnvJogo.SetActive (true);
 	}
-}
+
+	public static void TerminarJogo ()
+	{
+		Iniciado = false;
+
+		GameObject cnvFim = GameObject.FindGameObjectWithTag ("CanvasFim");
+		cnvFim.SetActive (false);
+		cnvFim.SetActive (true);
+		}
+	}
+	
