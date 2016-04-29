3d175bbf181e02aa143bbc10e8e1bac8a2278951
Jhonatanlotse <jhonatanlot30@gmail.com>
2016-04-29 13:14:43 -0300

    inimigo azul commit

---
+++ Assets/Scripts/InimigoAzul.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InimigoAzul : MonoBehaviour {
+
+	private Rigidbody2D body;
+
+	private Vector2 velocidade;
+
+	// Use this for initialization
+	void Start () {
+		velocidade = new Vector2 (0, -Random.Range (100, 250));
+		body.GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		body.velocity = velocidade * Time.deltaTime; 
+	}// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
